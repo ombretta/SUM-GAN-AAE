0dc16622984d3d180c5d9c539a8787cd619c8679
Ombretta Strafforello <ombretta.st@gmail.com>
2020-06-10 10:46:10 +0200

    commit

--- evaluation/check_fscores_summe.py
+++ evaluation/check_fscores_summe.py
@@ -5,13 +5,14 @@ import h5py
 from generate_summary import generate_summary
 from evaluation_metrics import evaluate_summary
 
-paths = ['../model/exp0_i3d/SumMe/results/split0', \
-         '../model/exp0_i3d/SumMe/results/split1', \
-         '../model/exp0_i3d/SumMe/results/split2', \
-        '../model/exp0_i3d/SumMe/results/split3', \
-        '../model/exp0_i3d/SumMe/results/split4'] # path to the json files with the computed importance scores for each epoch
+paths = ['../model/reg05_lr5_i3d/SumMe/results/split0', \
+         '../model/reg05_lr5_i3d/SumMe/results/split1', \
+         '../model/reg05_lr5_i3d/SumMe/results/split2', \
+        '../model/reg05_lr5_i3d/SumMe/results/split3', \
+        '../model/reg05_lr5_i3d/SumMe/results/split4'] # path to the json files with the computed importance scores for each epoch
 
 for path in [p for p in paths if "f_scores.txt" not in listdir(p)]:
+    print(path)
     results = [r for r in listdir(path) if "f_scores" not in r and ".json" in r]
     results.sort(key=lambda video: int(video[6:-5]))
     # PATH_SumMe = '../data/SumMe/eccv16_dataset_summe_google_pool5.h5'
--- evaluation/plot_fscores.py
+++ evaluation/plot_fscores.py
@@ -9,7 +9,8 @@ Created on Fri Jun  5 13:39:50 2020
 from matplotlib import pyplot as plt
 import numpy as np
 
-dataset = "SumMe"
+dataset = "TVSum"
+folder = "reg05_i3d"
 
 f_scores = {}
 f_scores_gts = {}
@@ -19,23 +20,23 @@ for score in ["f_scores", "f_scores_gts"]:
     average_scores = [0 for i in range(100)]
     
     figure = plt.figure(score)
-    for split in range(5):
-        fscores_path = "../model/exp0/"+dataset+"/results/split"+str(split)+"/"+score+".txt"
+    for split in range(4):
+        fscores_path = "../model/"+folder+"/"+dataset+"/results/split"+str(split)+"/"+score+".txt"
         
         with open(fscores_path, "r") as f:
             scores_per_epoch = f.read()
         scores_per_epoch = [float(s) for s in scores_per_epoch[1:-1].split(", ")]
         print("Split", score, str(split), str(round(max(scores_per_epoch), 2)), "at epoch", np.argmax(scores_per_epoch)+1)
         
-        for i in range(100): average_scores[i] += scores_per_epoch[i] 
+        for i in range(len(scores_per_epoch[1:])): average_scores[i] += scores_per_epoch[i]
         
         plt.plot(scores_per_epoch)
     
-    print("Average", score, str(round(max(average_scores)/5, 2)), "at epoch", np.argmax(average_scores)+1)
+    print("Average", score, str(round(max(average_scores)/4, 2)), "at epoch", np.argmax(average_scores)+1)
     plt.plot([a/5 for a in average_scores])
 
-    plt.legend(["split " + str(i) for i in range(5)]+["average"])    
+    plt.legend(["split " + str(i) for i in range(4)]+["average"])    
     plt.title(dataset+" - "+score)
     plt.xlabel("epoch")
     plt.ylabel("f-score")
-    plt.savefig("../model/exp0/"+dataset+"/results/"+score+".png")
+    plt.savefig("../model/"+folder+"/"+dataset+"/results/"+score+".png")

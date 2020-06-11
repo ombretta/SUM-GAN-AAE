351281acf50e2d6cf1221496eeb72c3f9607ced1
Ombretta Strafforello <ombrettastraff@login1.hpc.tudelft.nl>
2020-06-11 15:28:03 +0200

    commit

--- model/train_all_splits.py
+++ model/train_all_splits.py
@@ -30,23 +30,19 @@ text = "#!/bin/sh\n\
 #SBATCH --time=6:00:00\n\
 #SBATCH --ntasks=1\n\
 #SBATCH --cpus-per-task=2\n\
-#SBATCH --mem=16000\n\
+#SBATCH --mem=4000\n\
 #SBATCH --gres=gpu:1\n\
 module use /opt/insy/modulefiles\n\
 module load cuda/10.0 cudnn/10.0-7.6.0.64\n\
-srun python main.py --split_index= "
+srun python main.py --split_index "
 
 video_types = ["TVSum", "SumMe"]
 
 features_type = "i3d" #"google"
 
-regularization_factors = [0.05, 0.1, 0.15, 0.3]
+regularization_factors = [0.05, 0.1, 0.15, 0.3, 0.5]
 
-<<<<<<< HEAD
 lr = 1e-5
-=======
-lr = 1e-4
->>>>>>> 5bd54eccdbb88095178278dd4a27650f6340bff7
 
 for video_type in video_types:
     
@@ -59,9 +55,9 @@ for video_type in video_types:
         prepare_folder(save_dir, video_type)
         
         for split_index in range(5):
-            full_text = text + str(split_index) + " --video_type=" + video_type + \
-            " --features_type=" + features_type + " --regularization_factor=" + \
-            str(regularization_factor) + " --lr=" + str(lr) + " --save_dir=" + \
+            full_text = text + str(split_index) + " --video_type " + video_type + \
+            " --features_type " + features_type + " --regularization_factor " + \
+            str(regularization_factor) + " --lr " + str(lr) + " --save_dir " + \
             save_dir
             
             print(full_text)
